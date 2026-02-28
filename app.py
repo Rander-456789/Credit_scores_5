4b8a9d9a97bcd4e132fb5d3170fd918a114eaae3
Rander-456789 <Ilya.palkin2009@gmail.com>
2026-02-28 20:17:54 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import pandas as pd
+from prediction import predictions
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+
+FEATURES = [
+    "Age",
+    "Income",
+    "LoanAmount",
+    "NumCreditLines",
+    "InterestRate"
+]
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "result": None,
+            "form": {}
+        }
+    )
+
+
+@app.post("/predict", response_class=HTMLResponse)
+def predict(
+    request: Request,
+    Age: int = Form(...),
+    Income: float = Form(...),
+    LoanAmount: float = Form(...),
+    Education: str = Form(...), 
+    LoanPurpose: str = Form(...)
+    ):
+    df = pd.DataFrame([{
+        "Age": Age,
+        "Income": Income,
+        "LoanAmount": LoanAmount,
+        "Education": Education,
+        "LoanPurpose": LoanPurpose
+    }])
+
+    
+    result = predictions.pred(df)
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "result": result,
+            "form": df.iloc[0].to_dict()
+        }
+    )
---
+++ prediction.py
@@ -0,0 +1,50 @@
+import joblib
+from threading import Lock
+from lightgbm import LGBMClassifier
+import pandas as pd
+
+
+class Predictions:
+    def __init__(self):
+        self.lock = Lock()
+
+    def pred(self, data, flag=0.5):
+        model = joblib.load("model.pkl")
+
+        education_mapping = {
+            "Высшее образование (бакалавр)": 0,
+            "Высшее образование (магистр)": 1,
+            "Полное среднее образование": 2,
+            "Учёная степень": 3
+        }
+        data['Education'] = data["Education"].map(education_mapping)
+        data['LoanPurpose_Auto'] = False
+        data['LoanPurpose_Business'] = False
+        data['LoanPurpose_Education'] = False
+        data['LoanPurpose_Home'] = False
+
+        if data['LoanPurpose'].iloc[0] == 'Автомобиль': data['LoanPurpose_Auto'] = True
+        elif data['LoanPurpose'] == 'Бизнес': data['LoanPurpose_Business'] = True 
+        elif data['LoanPurpose'] == 'Образование': data['LoanPurpose_Education'] = True 
+        elif data['LoanPurpose'] == 'Недвижимость': data['LoanPurpose_Home'] = True
+
+        data['Inc*LoanAmount'] = data['Income'] * data['LoanAmount']
+        data['Inc*age'] = data['Income'] * data['Age']
+        data['LoanAmount*age'] = data['LoanAmount'] * data['Age']
+
+
+        age = data['Age']
+        income = data['Income']
+        loan = data['LoanAmount']
+
+        if (
+            18 <= age <= 100 and
+            income > 20000 and
+            500000 <= loan <= 25000000
+        ):
+            return int(model.predict_proba(data)[0][1] >= flag)
+        else:
+            return -1
+
+
+predictions = Predictions()
