ecea2ce07d56b14433d09c63dba9f9620e8f03b3
Rander-456789 <ilya.palkin2009@gmail.com>
2026-03-05 08:59:47 +0300

    add credit scoring app

---
+++ app.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import pandas as pd
+from prediction import predictions
+from money_validation import valid
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory=".")
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "result": None,
+            "form": {}
+        }
+    )
+
+
+@app.post("/predict", response_class=HTMLResponse)
+def predict(
+    request: Request,
+    Age: int = Form(...),
+    Income: float = Form(...),
+    LoanAmount: float = Form(...),
+    LoanTerm: int = Form(...),
+    Education: str = Form(...), 
+    LoanPurpose: str = Form(...)
+    ):
+    mon = [None, None, None, None]
+    df = pd.DataFrame([{
+        "Age": Age,
+        "Income": Income,
+        "LoanAmount": LoanAmount,
+        "LoanTerm": LoanTerm*12,
+        "Education": Education,
+        "LoanPurpose": LoanPurpose
+    }])
+
+    
+    result = predictions.pred(df)
+    if result == 0:
+        mon = valid.compil(df)
+
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "result": result,
+            "rate": mon[0],
+            "total_payment": mon[1],
+            "monthly_payment": mon[2],
+            "first_payment": mon[3],
+            "form": df.iloc[0].to_dict()
+        }
+    )
---
+++ money_validation.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from threading import Lock
+
+class Valid:
+    def __init__(self):
+        self.lock = Lock()
+
+    def compil(self, data):
+
+        return [15, 45546564, 50000, 200000]
+    
+
+valid = Valid()
